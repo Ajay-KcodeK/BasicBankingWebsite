d0e81c0be2569044f41e866293ab4be8134d08b8
Ajay <ajay.1822co1006@kiet.edu>
2021-02-21 02:03:05 +0530

    Initial commit

---
+++ contact.php
@@ -0,0 +1,96 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+    <!-- <style>
+      .contact{
+        background-color: rgb(167 173 178);
+	height: 650px;
+
+}
+
+
+.text-center{
+  text-align: center;
+  padding: 25px;
+  font-size: 46px;
+	color: #f8f8f8;
+}
+.form{
+  max-width: 62%;
+  margin: 27px auto;
+}
+
+.form-input{
+  width: 100%;
+  font-size: 16px;
+  border: 2px solid gray;
+  border-radius: 15px;
+  margin: 8px;
+  padding: 8px;
+  outline: none;
+  font-family: 'Ubuntu', sans-serif;
+}
+
+.btn-dark{
+	border: none;
+    border-radius: 8px;
+    padding: 10px;
+    background: #374a4b;
+    color: white;
+    letter-spacing: 1.5px;
+    font-size: 27px;
+    transition: 0.5s;
+}
+    </style> -->
+</head>
+
+<body>
+
+  
+    
+  <section class="contact" id="contact">
+    <h2 class="text-center one">Contact Us</h2>
+    <div class="form">
+      <input  class="form-input type="text" name="name" id="name" placeholder="Enter your name">
+      <input  class="form-input type="email" name="email" id="email" placeholder="Enter your email">
+      <input  class="form-input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+      <textarea class="form-input  name="text" id="text" cols="20" rows="10" placeholder="Mention your Concern"></textarea>
+      <button class="btn btn-dark">Submit</button>
+    </div>
+  </section>
+
+  <!-- <?php
+   //src="https://source.unsplash.com/500x400/?coding,python"
+  ?> -->
+
+ 
+<footer class="text-center mt-1 py-2 foot">
+        <!-- <p>Made by <b>Devika Lal</b> <br> The Sparks Foundation</p> -->
+        
+
+      <p class="content"> Copyright © www.tsfbank.com 2021 - All right reserved</p>
+    
+      </footer>
+      <script src="index.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,22 @@
+ <!-- navbar --> 
+ <nav class="navbar navbar-expand-md navbar-dark bg-dark  h-nav-resp">
+      <a class="navbar-brand" href="index.php">TSF-BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto v-class-resp">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+          </div>
+       </nav>
